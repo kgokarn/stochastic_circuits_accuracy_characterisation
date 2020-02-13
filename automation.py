ed6abfe4114e84d9e7c6c59f7f1db1616fab4622
kgokarn <kartik.gokarn@gmail.com>
2020-02-13 17:23:34 +0100

    mean square error implementation

--- automation.py
+++ automation.py
@@ -36,30 +36,32 @@ if(given_choice == '1'):
         circuit_simulation_list = continuous_simulation.circuit(snumber, circuit_simulation_list)
         ckt_trav_index = ckt_trav_index + 1
     print(circuit_simulation_list)
-    ##continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
-    stoc_number = acc_char.calc_stochastic_number(conf_level, err_tolerance)
+    mean_square_error = continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
+    stoc_number = acc_char.calc_stochastic_number(conf_level, err_tolerance, mean_square_error)
     print("stochastic number is: ", stoc_number)
 
 elif(given_choice == '2'):
     conf_level = float(input("Enter the confidence level: "))
     stoc_number = float(input("Enter the stochastic number: "))
-    snumber = stoc_number;
+    snumber = int(stoc_number);
     while (ckt_trav_index <= int(num_simulation) - 1):
         circuit_simulation_list = continuous_simulation.circuit(snumber, circuit_simulation_list)
         ckt_trav_index = ckt_trav_index + 1
     print(circuit_simulation_list)
-    err_tolerance = acc_char.calc_error_tolerance(conf_level, stoc_number)
+    mean_square_error = continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
+    err_tolerance = acc_char.calc_error_tolerance(conf_level, stoc_number, mean_square_error)
     print("error tolerance is: ", err_tolerance)
 
 elif(given_choice == '3'):
     err_tolerance = float(input("Enter the error tolerance: "))
     stoc_number = float(input("Enter the stochastic number: "))
-    snumber = stoc_number;
+    snumber = int(stoc_number);
     while (ckt_trav_index <= int(num_simulation) - 1):
         circuit_simulation_list = continuous_simulation.circuit(snumber, circuit_simulation_list)
         ckt_trav_index = ckt_trav_index + 1
     print(circuit_simulation_list)
-    conf_level = acc_char.calc_confidence_level(err_tolerance, stoc_number)
+    mean_square_error = continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
+    conf_level = acc_char.calc_confidence_level(err_tolerance, stoc_number, mean_square_error)
     print("confidence level is: ", conf_level)
 
 else:
