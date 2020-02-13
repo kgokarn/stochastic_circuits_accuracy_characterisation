6e55c9da6142f433a4ed51570289052027ba8f94
kgokarn <kartik.gokarn@gmail.com>
2020-02-13 17:22:41 +0100

    mean square error implementation

--- accuracy.py
+++ accuracy.py
@@ -1,12 +1,12 @@
 import math
 
 class accuarcy_characterization:
-    def calc_stochastic_number(self, conf_level, err_tolerance):
-        return math.ceil(9/(conf_level* pow(err_tolerance, 2)))
+    def calc_stochastic_number(self, conf_level, err_tolerance, mean_square_error):
+        return math.ceil(mean_square_error/((1 - conf_level)* pow(err_tolerance, 2)))
 
-    def calc_error_tolerance(self, conf_level, stoc_number):
-        return 3/(math.sqrt(stoc_number * conf_level))
+    def calc_error_tolerance(self, conf_level, stoc_number, mean_square_error):
+        return math.sqrt(mean_square_error)/(math.sqrt(stoc_number * (1 - conf_level)))
 
-    def calc_confidence_level(self, err_tolerance, stoc_number):
-        return 9/(stoc_number * pow(err_tolerance, 2))
+    def calc_confidence_level(self, err_tolerance, stoc_number, mean_square_error):
+        return 1 - (mean_square_error/(stoc_number * pow(err_tolerance, 2)))
 
