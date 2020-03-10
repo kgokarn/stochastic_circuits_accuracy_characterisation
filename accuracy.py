b5fa0b2c904ae39415c2fd7818dc448055fb10d4
kgokarn <kartik.gokarn@gmail.com>
2020-03-10 16:44:17 +0100

    Relation between parameters is changed to CLT. Comments are added

--- accuracy.py
+++ accuracy.py
@@ -1,12 +1,13 @@
 import math
-
+from scipy.stats import norm
+#CLT relation between stochastic number, error tolernace and confidence level
 class accuarcy_characterization:
-    def calc_stochastic_number(self, conf_level, err_tolerance, mean_square_error):
-        return math.ceil(mean_square_error/((1 - conf_level)* pow(err_tolerance, 2)))
+    def calc_stochastic_number(self, conf_level, err_tolerance, mean_square_error, num_simulation):
+        return math.ceil(math.pow(norm.ppf(norm.cdf((1 - (1 - conf_level)/2 ))) * math.sqrt(mean_square_error * int(num_simulation))/err_tolerance, 2))
 
-    def calc_error_tolerance(self, conf_level, stoc_number, mean_square_error):
-        return math.sqrt(mean_square_error)/(math.sqrt(stoc_number * (1 - conf_level)))
+    def calc_error_tolerance(self, conf_level, stoc_number, mean_square_error, num_simulation):
+        return norm.ppf(norm.cdf((1 - (1 - conf_level)/2 ))) * math.sqrt((int(num_simulation) * mean_square_error)/stoc_number)
 
-    def calc_confidence_level(self, err_tolerance, stoc_number, mean_square_error):
-        return 1 - (mean_square_error/(stoc_number * pow(err_tolerance, 2)))
+    def calc_confidence_level(self, err_tolerance, stoc_number, mean_square_error, num_simulation):
+        return 2 * norm.cdf(math.sqrt((mean_square_error * int(num_simulation))/ stoc_number) * err_tolerance) - 1
 
