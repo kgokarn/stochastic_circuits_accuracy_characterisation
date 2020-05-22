795e99b1af9fe48bd54fdcb82c7a0badcfc35ba3
kgokarn <kartik.gokarn@gmail.com>
2020-05-22 22:31:13 +0200

    Unipolar SC automation script - submission

--- automation.py
+++ automation.py
@@ -19,12 +19,11 @@ acc_char = accuarcy_characterization()
 
 
 #To choose from either of the combinations
-print ("1 : Confidence Level and Error Tolerance is given\n"
-       "2 : Confidence Level and Stochastic Number is given\n"
-       "3 : Error Tolerance and Stochastic Number is given\n")
+print ("1 : Confidence Level and Precision is given\n"
+       "2 : Confidence Level and length of Stochastic Number is given\n"
+       "3 : Precision and length of Stochastic Number is given\n")
 
 given_choice = input("Enter the option that is given: ")
-print(given_choice)
 
 num_simulation = input("Enter the number of times the circuit needs to run: ")
 circuit_simulation_list = []
@@ -33,19 +32,19 @@ ckt_trav_index = 0
 if(given_choice == '1'):
     snumber = 32;                                                          ##Default Stochastic Number chosen to be 32
     conf_level = float(input("Enter the confidence level: "))
-    err_tolerance = float(input("Enter the error tolerance: "))
+    precision = float(input("Enter the precision: "))
     while (ckt_trav_index <= int(num_simulation) - 1):
         circuit_simulation_list = continuous_simulation.circuit(snumber, circuit_simulation_list)
         ckt_trav_index = ckt_trav_index + 1
     print("circuit simulation result is: ",circuit_simulation_list)
     mean_square_error = continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
     print("Mean Square Error is: ", mean_square_error)
-    stoc_number = acc_char.calc_stochastic_number(conf_level, err_tolerance, mean_square_error, num_simulation)
-    print("stochastic number is: ", stoc_number)
+    stoc_number = acc_char.calc_stochastic_number(conf_level, precision, mean_square_error, num_simulation)
+    print("optimal length of stochastic number is: ", stoc_number)
 
 elif(given_choice == '2'):
     conf_level = float(input("Enter the confidence level: "))
-    stoc_number = float(input("Enter the stochastic number: "))
+    stoc_number = float(input("Enter the length of stochastic number: "))
     snumber = int(stoc_number);
     while (ckt_trav_index <= int(num_simulation) - 1):
         circuit_simulation_list = continuous_simulation.circuit(snumber, circuit_simulation_list)
@@ -53,12 +52,12 @@ elif(given_choice == '2'):
     print("circuit simulation result is: ",circuit_simulation_list)
     mean_square_error = continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
     print("Mean Square Error is: ", mean_square_error)
-    err_tolerance = acc_char.calc_error_tolerance(conf_level, stoc_number, mean_square_error, num_simulation)
-    print("error tolerance is: ", err_tolerance)
+    precision = acc_char.calc_precision(conf_level, stoc_number, mean_square_error, num_simulation)
+    print("precision is: ", precision)
 
 elif(given_choice == '3'):
-    err_tolerance = float(input("Enter the error tolerance: "))
-    stoc_number = float(input("Enter the stochastic number: "))
+    precision = float(input("Enter the precision: "))
+    stoc_number = float(input("Enter the length of stochastic number: "))
     snumber = int(stoc_number);
     while (ckt_trav_index <= int(num_simulation) - 1):
         circuit_simulation_list = continuous_simulation.circuit(snumber, circuit_simulation_list)
@@ -66,7 +65,7 @@ elif(given_choice == '3'):
     print("circuit simulation result is: ",circuit_simulation_list)
     mean_square_error = continuous_simulation.mean_square_error(function_expression, circuit_simulation_list)
     print("Mean Square Error is: ", mean_square_error)
-    conf_level = acc_char.calc_confidence_level(err_tolerance, stoc_number, mean_square_error, num_simulation)
+    conf_level = acc_char.calc_confidence_level(precision, stoc_number, mean_square_error, num_simulation)
     print("confidence level is: ", conf_level)
 
 else:
